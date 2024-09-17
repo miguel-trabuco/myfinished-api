5631a282274ef1d2c76b84566acda6e05dbce04f
Miguel Trabuco <migueltrabuco07@gmail.com>
2024-09-16 21:55:22 -0300

    Add app configuration
    Add json, cors, dotenv and cookie-parser configuration

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 //Import types
 import { Application } from 'express';
 
 const app: Application = express();
 
-app.listen(8090, () => {
+//App config
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+const PORT: string = process.env.PORT as string;
+app.listen(PORT, () => {
 	console.log('Server on');
 });
