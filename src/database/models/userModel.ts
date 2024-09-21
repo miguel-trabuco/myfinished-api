59988095d0e241dd5fc63456d99bf80eff478ae3
Miguel Trabuco <migueltrabuco07@gmail.com>
2024-09-21 19:33:24 -0300

    Update user model
    Change password to passwordHash in schema

--- src/database/models/userModel.ts
+++ src/database/models/userModel.ts
@@ -4,7 +4,7 @@ const UserSchema: mongoose.Schema<IUser> = new mongoose.Schema({
 	id: { type: String, required: true, unique: true },
 	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true }
+	passwordHash: { type: String, required: true }
 });
 
 const UserModel = mongoose.model<IUser>('User', UserSchema);
