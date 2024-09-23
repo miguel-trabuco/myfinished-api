31baa0843f74ccb0ab2e8eed952b9fb635f7aa85
Miguel Trabuco <migueltrabuco07@gmail.com>
2024-09-23 08:33:08 -0300

    Update all models
    Remove id to change to default mongoDB id (_id)

--- src/database/models/itemModel.ts
+++ src/database/models/itemModel.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import type { IItem } from '../../interfaces/IItem';
 
 const ItemSchema: mongoose.Schema<IItem> = new mongoose.Schema({
-	id: { type: String, required: true, unique: true },
 	name: { type: String, required: true },
 	listID: { type: String, required: true },
 	status: { type: String, required: true }
--- src/database/models/listModel.ts
+++ src/database/models/listModel.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import type { IList } from '../../interfaces/IList';
 
 const ListSchema: mongoose.Schema<IList> = new mongoose.Schema({
-	id: { type: String, required: true, unique: true },
 	name: { type: String, required: true },
 	userID: { type: String, required: true },
 	itemsAmount: { type: Number, required: true }
