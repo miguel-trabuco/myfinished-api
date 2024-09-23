4805d585f0d950a31be9c084552d5bb83e88f1c9
Miguel Trabuco <migueltrabuco07@gmail.com>
2024-09-23 05:55:12 -0300

    Add update user route

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -3,8 +3,11 @@ import { Router } from 'express';
 //Import controllers
 import { UserController } from '../controllers/UserController';
 
+import verifyTokenMiddleware from '../middlewares/verifyTokenMiddleware';
+
 const userRouter: Router = Router();
 
 userRouter.post('/create', UserController.createUser);
+userRouter.put('/update', verifyTokenMiddleware, UserController.updateUser);
 
 export default userRouter;
