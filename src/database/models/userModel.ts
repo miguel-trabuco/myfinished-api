a787272bb3233d80ff710d8e7a24ad454bb9b5b1
Miguel Trabuco <migueltrabuco07@gmail.com>
2024-09-30 21:43:36 -0300

    Add id in user model

--- src/database/models/userModel.ts
+++ src/database/models/userModel.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import type { IUser } from '../../interfaces/IUser';
 const UserSchema: mongoose.Schema<IUser> = new mongoose.Schema({
+	id: { type: String, required: true },
 	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	passwordHash: { type: String, required: true }
