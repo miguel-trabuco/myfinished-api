e3be4c1757407d3ec5302247c311f55fcf4f96a2
Miguel Trabuco <migueltrabuco07@gmail.com>
2024-10-24 15:55:22 -0300

    Create response messages object

---
+++ src/util/responseMessages.ts
@@ -0,0 +1,12 @@
+export const responseMessages = {
+	MISSING_PARAMETERS: (params: string[]) => `Missing parameters: ${params.join(', ')}`,
+	PASSWORD_LENGTH: 'Password must be betwee 6 and 500 character',
+	SERVER_ERROR: 'Internal server error',
+	USER_NOT_FOUND: 'User not found',
+	WRONG_PASSWORD: 'Wrong password',
+	UNAUTHORIZED: 'Unauthorized',
+	USER_CREATED: 'User created',
+	USER_UPDATED: 'User updated',
+	USER_DELETED: 'User deleted',
+	LOGGED: 'Logged'
+};
